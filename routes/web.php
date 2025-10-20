111abc3dfdbd1721cde2e924538a193ab611e474
Loïc Leforestier <loicleforestier.pro@gmail.com>
2025-10-20 23:16:41 +0200

    add wallpaper migration and model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use \App\Models\Wallpaper;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -11,6 +12,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/wallpaper', function () {
+    $wallpaper = Wallpaper();
+//    $wallpaper->
+
+//    return '';
 });
 
 require __DIR__.'/settings.php';
